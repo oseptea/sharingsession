09e5513646c5b22999ed49f33cfe9d9c14079a29
Yusuf Surahman <yusuf@infoflow.co.id>
2018-04-18 15:07:05 +0700

    remove unused import

--- src/test/java/com/sharingsession/controller/EmployeeRestControllerIntegrationTest.java
+++ src/test/java/com/sharingsession/controller/EmployeeRestControllerIntegrationTest.java
@@ -18,7 +18,6 @@ import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -27,7 +26,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.sharingsession.Application;
-import com.sharingsession.controller.EmployeeRestController;
 import com.sharingsession.model.Employee;
 import com.sharingsession.repository.EmployeeRepository;
 import com.sharingsession.utils.JsonUtil;
@@ -35,9 +33,8 @@ import com.sharingsession.utils.JsonUtil;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = Application.class)
 @AutoConfigureMockMvc
-@AutoConfigureTestDatabase
 public class EmployeeRestControllerIntegrationTest {
-
+	
     @Autowired
     private MockMvc mvc;
 
